3918e00f4a2e26623072fdcd6a3e8620694d0b53
TalimeQ <boren1996@wp.pl>
2025-03-31 12:58:37 +0200

    Stats counting

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ def get_book_text(file_path):
         file_contents = file.read()
         return file_contents
 
+def get_num_words(parsed_string):
+    words_array = parsed_string.split()
+    return len(words_array)
 
 def main():
-    data_to_print = get_book_text("books/frankenstein.txt")
-    print(data_to_print)
+    data_to_parse = get_book_text("books/frankenstein.txt")
+    num_words = get_num_words(data_to_parse)
+    print(f"{num_words} words found in the document")
 
 
 main()
